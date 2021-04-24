98f545db7b58e3d1f4e09609f18bb684fa82bc5a
Yichun Zhang <yichun.zhang@romtech.com>
2021-04-24 09:38:53 -0400

    clean up code

--- app/src/main/java/com/example/myplaces/api/PlaceListAPIService.kt
+++ app/src/main/java/com/example/myplaces/api/PlaceListAPIService.kt
@@ -10,9 +10,6 @@ import retrofit2.http.*
 interface PlacesListAPIService {
     @GET("/maps/api/place/nearbysearch/json")
     fun fetchplaces(@Query("keyword") keyword:String, @Query("key") key:String, @Query("location") latlon:String, @Query("radius") radius:String): Deferred<Response<PlacesListReponse>>
-
-    @GET("/maps/api/place/photo")
-    fun fetchphoto(@Query("key") key:String, @Query("photoreference") photoreference:String, @Query("maxheight") maxheight:String, @Query("maxwidth") maxwidth:String): Deferred<Response<PlacesListReponse>>
 }
 
 object PlacesListAPI {
--- app/src/main/java/com/example/myplaces/api/PlacesListRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myplaces.api
-
-import com.squareup.moshi.Json
-
-
